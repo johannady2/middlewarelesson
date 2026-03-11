6e1766dd66b54002d7a594560bab1494b36e0428
johannady2 <johannady2@gmail.com>
2026-03-11 11:54:18 +0900

    added morgan middelware to index2.js

--- index2.js
+++ index2.js
@@ -1,10 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import { dirname } from "path";//this is needed to get the directory name of the current module
+import { fileURLToPath } from "url"; //this converts the URL to a file path
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+
+app.use(morgan("tiny", (req,res) => {
+  console.log(`tesing: ${req.method} ${req.url} ${res.statusCode}`);
+}));
+
+
+
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  //res.send("Hello");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
