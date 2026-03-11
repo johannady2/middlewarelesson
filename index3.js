5d62260f6db48f1cc556e5d0904a823d351d8cf8
johannady2 <johannady2@gmail.com>
2026-03-11 12:11:39 +0900

    made my own middleware on index3.js

--- index3.js
+++ index3.js
@@ -1,12 +1,25 @@
 import express from "express";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+
+  console.log(`req.method: ${req.method} , req.url:${req.url}, statusCode: ${res.statusCode}  `);
+  next();
+}
+
 app.use(logger);
 
+
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  //res.send("Hello");
+  res.sendFile(__dirname + "/public/index.html");
+
 });
 
 app.listen(port, () => {
